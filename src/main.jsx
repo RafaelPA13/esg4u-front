9e2297c76ac6998b68a5ccb372bf2ca7133e9c2d
RafaelPA13 <rafaporann@gmail.com>
2026-04-13 12:12:46 -0300

    feat: Rotas de Autenticação e Landing Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './App.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Páginas
+import LandingPage from "./pages/LandingPage";
+
+// Páginas de Autenticação
+import Login from "./pages/auth/Login";
+import Cadastro from "./pages/auth/Cadastro";
+
+// Rotas
+import AuthRoutes from "./routes/AuthRoutes";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/autenticacao",
+    element: <AuthRoutes />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "cadastro",
+        element: <Cadastro />,
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
