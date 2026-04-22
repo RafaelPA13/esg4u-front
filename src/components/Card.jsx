f721cc577a4d9844b029efede1f4be04bdeea974
RafaelPA13 <rafaporann@gmail.com>
2026-04-22 18:20:49 -0300

    fix: Ajuste na sombra do card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,3 @@
 export default function Card({ children, row = false }) {
-  return <li className={`bg-slate-50 border border-slate-200 shadow-lg p-6 rounded-xl flex gap-3 ${row ? "flex-row items-center" : "flex-col"}`}>{children}</li>;
+  return <li className={`bg-slate-50 border border-slate-200 shadow p-6 rounded-xl flex gap-3 ${row ? "flex-row items-center" : "flex-col"}`}>{children}</li>;
 }
