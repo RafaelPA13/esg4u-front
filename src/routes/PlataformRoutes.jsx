0c60b0c1b8ddd70d3cd37fd87fa5f3622d09f5ee
RafaelPA13 <rafaporann@gmail.com>
2026-04-22 17:46:35 -0300

    feat: Mensagem de boas vindas

--- src/routes/PlataformRoutes.jsx
+++ src/routes/PlataformRoutes.jsx
@@ -20,7 +20,11 @@ export default function PlataformRoutes() {
   return (
     <div className="min-h-screen bg-slate-100">
       <NavbarPlataform />
-      <main className="pb-16 md:pl-20">
+      <main className="pb-16 px-8 pt-8 flex flex-col gap-6 md:w-[70%] md:mx-auto md:pb-0 md:px-0">
+        <div>
+          <h1 className="text-2xl font-bold">Olá, {user.nome || "Usuário"}!</h1>
+          <p className="text-slate-500">Acompanhe sua jornada ESG hoje.</p>
+        </div>
         <Outlet />
       </main>
     </div>
