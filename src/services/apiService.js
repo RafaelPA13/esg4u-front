d711cd177d3cc3ed84c69ded4acbb6f9da4d086d
RafaelPA13 <rafaporann@gmail.com>
2026-06-03 17:26:36 -0300

    feat: URL de produção

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000"; // TODO: Adicionar url do backend hospedado
+const API_BASE_URL = "https://web-production-92eb.up.railway.app/" || "http://localhost:8000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
